71c345070322645628578ad317c7046e7ce730b4
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-23 12:52:57 +0100

    update head

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,14 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Notify.is</title>
-        <link rel="icon" href="/favicon.ico" />
+
         <meta name="author" content="Oliver Proud" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>

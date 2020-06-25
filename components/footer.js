0235c1f93eb156da0468d741d4dacdb086f03711
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-25 20:58:14 +0100

    create footer and layout components

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://oliverproud.com" target="_blank">Built by Oliver Proud.</a>
+    </footer>
+  )
+}

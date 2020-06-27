c11e5b019095bfde7ac5a483c3eb340044a7136b
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-27 17:46:48 +0100

    add main section, has display:flex and flex:1 - pushes footer down

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Nav from './nav'
 import Footer from './footer'
+import layout from '../styles/layout.module.css'
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className={layout.container}>
       <Nav />
 
       {children}
+
+      <main className={layout.main}></main>
 
       <Footer />
     </div>

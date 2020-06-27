5c3ceb56595ce73448d1015e96afcaf658981abc
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-27 21:51:44 +0100

    underline contact link in privacy policy

--- pages/privacy.js
+++ pages/privacy.js
@@ -26,7 +26,7 @@ export default function Privacy() {
         <p className="p-privacy">We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
         <p className="p-privacy">Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
         <p className="p-privacy">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-        <p className="p-privacy">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+        <p className="p-privacy">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to <Link href="/"><a className="privacy-contact">contact us.</a></Link></p>
         <p className="p-privacy">This policy is effective as of 25 June 2020.</p>
 
 
@@ -36,6 +36,12 @@ export default function Privacy() {
             font-weight: normal;
             margin-top: 20px;
             width: 100%;
+          }
+          .privacy-contact {
+            text-decoration: underline;
+          }
+          .privacy-contact:hover {
+            text-decoration: none;
           }
           `}
         </style>

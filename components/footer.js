9033e50670325d5a1565e95919a9114715e75cdf
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-27 15:15:17 +0100

    update footer component

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,33 @@
-import styles from '../styles/footer.module.css'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <a href="https://oliverproud.com" target="_blank">Built by Oliver Proud.</a>
+    <footer className="footer border-top">
+      <ul className="footer-menu">
+        <li>
+          &copy; Notify.is 2020
+        </li>
+        <li>
+          <Link href="#">
+            <a>Privacy</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="#">
+            <a>Terms</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="#">
+            <a>Contact</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="#">
+            <a>Delete my info</a>
+          </Link>
+        </li>
+      </ul>
     </footer>
   )
 }

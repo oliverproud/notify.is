17a5bb74d7f99f7315b733caee1ccc37a8a7d8cd
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-29 12:52:12 +0100

    update new navigation bar

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,56 @@
 import Link from 'next/link'
 
-export default function Nav() {
-  return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
-      <Link href="/">
-        <a className="my-0 mr-md-auto">
-          <img src="/notify-logo.svg" width="30" height="30" alt="notify logo" />
-        </a>
-      </Link>
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
-      <nav className="my-3 my-md-0 mr-md-3">
-        <Link href="/about"><a className="p-2">About</a></Link>
-        <Link href="/contact"><a className="p-2">Contact us</a></Link>
-      </nav>
-      <Link href="/signup"><a className="btn nav-btn">Get notified</a></Link>
-    </div>
+
+export default function Navigation() {
+  return (
+    <Navbar expand="lg">
+      <Navbar.Brand>
+        <Link href="/">
+          <a>
+            <img src="/notify-logo.svg" width="30" height="30" alt="notify logo" />
+          </a>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-nav-icon"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Link href="/about"><a className="p-2">About</a></Link>
+          <Link href="/contact"><a className="p-2">Contact us</a></Link>
+          <Link href="/signup"><a className="btn nav-btn">Get notified</a></Link>
+        </Nav>
+      </Navbar.Collapse>
+
+      <style jsx>{`
+        .nav-btn {
+          border-color: #333;
+          color: inherit;
+          margin-left: .5rem!important;
+        }
+
+        .nav-btn:hover {
+          background-color: #333;
+          border-color: #333;
+          color: #FFF;
+        }
+            `}
+      </style>
+      <style jsx global>{`
+        .custom-nav-icon.navbar-toggler {
+          border-color: transparent;
+        }
+
+        .navbar-toggler-icon {
+          background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgb(51,3,0)' stroke-linecap='square' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")!important;
+        }
+            `}
+      </style>
+
+    </Navbar>
+
+
   )
 }

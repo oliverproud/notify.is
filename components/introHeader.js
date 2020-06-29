98be853c94b3187199dca496074fd144f1b9c9d1
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-29 16:59:48 +0100

    update left and right padding

--- components/introHeader.js
+++ components/introHeader.js
@@ -1,6 +1,6 @@
 export default function IntroHeader({ children }) {
   return (
-    <div className="intro-header px-5 py-5 pt-md-5 pb-md-5 mx-auto">
+    <div className="intro-header px-4 py-5 pt-md-5 pb-md-5 mx-auto">
       {children}
     </div>
   )

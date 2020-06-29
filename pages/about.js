cbe0294abb27f20261d86b43cf0ba68f40056df6
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-29 13:21:59 +0100

    cleanup

--- pages/about.js
+++ pages/about.js
@@ -34,7 +34,6 @@ export default function About() {
             color: inherit;
             margin-left: .5rem!important;
           }
-
           .about-btn:hover {
             background-color: #333;
             border-color: #333;

556e028b4cb9cffafa77c5a1777b23872f7c51f4
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-29 21:13:25 +0100

    test updated footer

--- components/footer.js
+++ components/footer.js
@@ -3,26 +3,26 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className="footer">
-      <ul className="footer-items">
-        <li>
-          <span>&copy; Notify.is 2020</span>
+      <ul className="list-inline footer-items">
+        <li className="list-inline-item">
+          <span>&copy; Notify.is 2020 |</span>
         </li>
-        <li>
+        <li className="list-inline-item">
           <Link href="/privacy">
-            <a>Privacy</a>
+            <a>Privacy |</a>
           </Link>
         </li>
-        <li>
+        <li className="list-inline-item">
           <Link href="/tos">
-            <a>Terms</a>
+            <a>Terms |</a>
           </Link>
         </li>
-        <li>
+        <li className="list-inline-item">
           <Link href="/contact">
-            <a>Contact us</a>
+            <a>Contact us |</a>
           </Link>
         </li>
-        <li>
+        <li className="list-inline-item">
           <Link href="/delete">
             <a>Delete my info</a>
           </Link>

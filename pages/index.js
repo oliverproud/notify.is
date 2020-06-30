de19bf5174ec03432da49bab6055f0589e9a3f78
Oliver Proud <oliverproud@users.noreply.github.com>
2020-06-30 12:06:57 +0100

    remove card, add button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import Button from 'react-bootstrap/Button'
 
 export default function Home() {
   return (
@@ -9,24 +10,37 @@ export default function Home() {
       <div className="intro-header px-4 py-3 pt-5 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 className="display-4">Notify.is</h1>
         <p>Get notified when your favourite username on Instagram becomes available.</p>
+        <Link href="/signup">
+          <a>
+            <Button className="signup-btn mt-2" size="lg">
+              Get notified
+            </Button>
+          </a>
+        </Link>
       </div>
 
-      <div className="container">
-        <div className="card mb-4 shadow-sm text-center">
-          <div className="card-body">
-            <h1 className="card-title card-title">
-              <small>You give us</small>
-            </h1>
-            <ul className="list-unstyled mt-3 mb-4">
-              <li>Your name</li>
-              <li>Email address</li>
-              <li>Unavailable username</li>
-            </ul>
-            <Link href="/signup"><a className="btn btn-lg signup-btn">Get notified</a></Link>
-          </div>
-        </div>
-      </div>
+      <style jsx>{`
+          .p-signup {
+            font-size: 18px;
+          }
+          `}
+        </style>
 
+        <style jsx global>{`
+            .signup-btn {
+              background-color: #333!important;
+              border-color: #333!important;
+              padding: 1rem 2rem!important;
+              color: #FFF!important;
+            }
+
+            .signup-btn:hover {
+              background-color: #FFF!important;
+              border-color: #333!important;
+              color: inherit!important;
+            }
+            `}
+          </style>
     </Layout>
   )
 }

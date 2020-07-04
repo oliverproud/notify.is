0897d32f03e5b311997f75f8c3f4aa364851c4dc
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-04 14:11:18 +0100

    test api with retrieving form data

---
+++ api/signup.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/api/signup" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+	firstName := r.FormValue("firstname")
+	lastName := r.FormValue("firstname")
+	email := r.FormValue("email")
+	username := r.FormValue("username")
+	fmt.Fprintf(w, "First name = %s\n", firstName)
+	fmt.Fprintf(w, "Last name = %s\n", lastName)
+	fmt.Fprintf(w, "Email address = %s\n", email)
+	fmt.Fprintf(w, "Username = %s\n", username)
+
+}

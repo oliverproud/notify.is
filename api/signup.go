997f5efc5064bd45e3c616d48199e59d75998d35
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-04 16:14:03 +0100

    clean up code, rename items

--- api/signup.go
+++ api/signup.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 )
 
-type ContactDetails struct {
+// SignupDetails parses the form values
+type SignupDetails struct {
 	firstName string
 	lastName  string
 	email     string
 	username  string
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+// SignupForm exposes an API endpoint to send POST requests to
+func SignupForm(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/api/signup" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -22,18 +24,18 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-	details := ContactDetails{
+
+	details := SignupDetails{
 		firstName: r.FormValue("firstname"),
 		lastName:  r.FormValue("lastname"),
 		email:     r.FormValue("email"),
 		username:  r.FormValue("username"),
 	}
+
+	fmt.Fprintf(w, "Post request r.PostFrom = %v\n", r.PostForm)
 	fmt.Fprintf(w, "First name = %s\n", details.firstName)
 	fmt.Fprintf(w, "Last name = %s\n", details.lastName)
 	fmt.Fprintf(w, "Email address = %s\n", details.email)
 	fmt.Fprintf(w, "Username = %s\n", details.username)
-
-	w.Write([]byte(fmt.Sprintf("%v", details)))
 
 }

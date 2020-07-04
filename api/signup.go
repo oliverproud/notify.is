84edf46c33ef91d257e2dbaf230f6065c0a9f768
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-04 17:59:41 +0100

    test using database

--- api/signup.go
+++ api/signup.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"notify.is-go/database"
 )
 
 // SignupDetails parses the form values
@@ -13,12 +16,24 @@ type SignupDetails struct {
 	username  string
 }
 
+const (
+	host   = "oliverproud.ddns.net"
+	port   = 5432
+	user   = "oliverproud"
+	dbname = "notify"
+)
+
 // SignupForm exposes an API endpoint to send POST requests to
 func SignupForm(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/api/signup" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, os.Getenv("DB_PASSWORD"), dbname)
+	database.InitDB(psqlInfo)
+
+	defer database.CloseDB()
 	// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -36,5 +51,12 @@ func SignupForm(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Last name = %s\n", details.lastName)
 	fmt.Fprintf(w, "Email address = %s\n", details.email)
 	fmt.Fprintf(w, "Username = %s\n", details.username)
+
+	result, err := database.InsertUser(details.firstName, details.lastName, details.email, details.username)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "User inserted into db %s\n", result)
 
 }

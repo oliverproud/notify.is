51df692f97a0e4238ccb19e2acf3f9cd1cef6312
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-04 19:03:27 +0100

    remove env, put password in temp const file

---
+++ api/const.go
@@ -0,0 +1,9 @@
+package handler
+
+const (
+	host     = "34.71.218.171"
+	port     = 5432
+	user     = "postgres"
+	password = "4wh7LbTjQJGM8NDxwkaYBKoeXJihYzUCNaLHpXENVhoVqbuZodKaxLBMFAkqNaVJ"
+	dbname   = "notify"
+)

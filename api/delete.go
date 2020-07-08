264598f236495d5b81fb21ecdefa70d5d4466232
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-08 20:33:13 +0100

    update to send confirmation email and then delete info

--- api/delete.go
+++ api/delete.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-
+	"net/url"
+	
 	"notify.is/database"
+	"notify.is/sendgrid"
 )
 
 // DeletionDetails parses the form values
@@ -23,12 +25,28 @@ func DeleteForm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), port, user, os.Getenv("DB_PASSWORD"), dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", os.Getenv("DB_HOST"), port, user, os.Getenv("DB_PASSWORD"), dbname)
 	database.InitDB(psqlInfo)
 
 	switch r.Method {
 	case "GET":
-		http.Redirect(w, r, "/delete", http.StatusSeeOther)
+		// http.Redirect(w, r, "/delete", http.StatusSeeOther)
+
+		// Get parameters from URL
+		keys, ok := r.URL.Query()["id"]
+		if !ok || len(keys[0]) < 1 {
+			return
+		}
+		fmt.Fprintf(w, "Parameter IDs:%v\n", keys)
+
+		for _, v := range keys {
+			result, err := database.DeleteUser(v)
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Fprintf(w, "%v\n", result)
+		}
+
 	case "POST":
 
 		if r.Body != http.NoBody {
@@ -49,12 +67,35 @@ func DeleteForm(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Last name = %s\n", details.lastName)
 			fmt.Fprintf(w, "Email address = %s\n", details.email)
 
-			result, err := database.DeleteUser(details.firstName, details.lastName, details.email)
+			var id []uint8
+			rows, err := database.GetUsers(details.email)
 			if err != nil {
-				log.Printf("%v", err)
+				log.Fatal(err)
 			}
 
-			fmt.Fprintf(w, "%s", result)
+			// Base URL that will have encoded parameters appended to
+			base, err := url.Parse("https://notify-is-git-development.oliverproud.vercel.app/api/delete")
+			if err != nil {
+				return
+			}
+			// Query params
+			params := url.Values{}
+
+			// Loop through rows returned by DB query
+			for rows.Next() {
+				err := rows.Scan(&id)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// Convert DB IDs to strings, add as parameters to URL values
+				params.Add("id", string(id))
+			}
+			// Create final URL using base URL and encoded parameters
+			base.RawQuery = params.Encode()
+			fmt.Println(base)
+
+			sendgrid.SendEmail(details.email, details.firstName, "", base.String(), "delete")
+
 		} else {
 			fmt.Fprintf(w, "Request body is empty. No records deleted.")
 		}
@@ -62,3 +103,9 @@ func DeleteForm(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Only GET and POST methods are supported.")
 	}
 }
+
+
+// func main() {
+// 	http.HandleFunc("/api/delete/", DeleteForm)
+// 	http.ListenAndServe(":***REMOVED***", nil)
+// }

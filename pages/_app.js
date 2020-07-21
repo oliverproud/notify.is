8a4fb382f0ab19a13553e54d28168daab00e48ef
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-21 15:26:40 +0100

    add env variable

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 import * as Sentry from '@sentry/react';
 
-Sentry.init({dsn});
+Sentry.init({dsn: process.ENV.SENTRY_DSN});
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />

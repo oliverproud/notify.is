7ec8f6903a7dbee07db4f430998e516f5fe4df56
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-21 15:30:43 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 import * as Sentry from '@sentry/react';
 
-Sentry.init({dsn: process.ENV.SENTRY_DSN});
+Sentry.init({dsn: process.env.SENTRY_DSN});
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />

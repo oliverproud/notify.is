6c80e00855627d83af33519705e462b25e7d528e
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-26 21:50:10 +0100

    update to insert services

--- database/insertUsers.go
+++ database/insertUsers.go
@@ -3,15 +3,15 @@ package database
 import "fmt"
 
 //InsertUser inserts a new user into the database table 'users'
-func InsertUser(firstName, lastName, email, username string) (string, error) {
+func InsertUser(firstName, lastName, email, username string, instagram, twitter bool) (string, error) {
 
 	sqlStatement := `
-  INSERT INTO users (first_name, last_name, email, username)
-  VALUES ($1, $2, $3, $4)
+  INSERT INTO users (first_name, last_name, email, username, instagram, twitter)
+  VALUES ($1, $2, $3, $4, $5, $6)
   RETURNING id`
 
 	var id []uint8
-	err := db.QueryRow(sqlStatement, firstName, lastName, email, username).Scan(&id)
+	err := db.QueryRow(sqlStatement, firstName, lastName, email, username, instagram, twitter).Scan(&id)
 	if err != nil {
 		return "", err
 	}

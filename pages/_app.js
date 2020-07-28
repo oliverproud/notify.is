64a3a2925bef227f65d887418d62cd55b42e6b4d
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-28 19:39:13 +0100

    testing Sentry as Vercel integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
-import * as Sentry from '@sentry/react';
-
-Sentry.init({dsn: process.env.SENTRY_DSN});
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />

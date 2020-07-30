b207564a25b7e78c78ca587f1549c64484b31987
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-30 17:02:10 +0100

    update db insert for github

--- database/insertUsers.go
+++ database/insertUsers.go
@@ -6,15 +6,15 @@ import (
 )
 
 //InsertUser inserts a new user into the database table 'users'
-func InsertUser(db *sql.DB, firstName, lastName, email, username string, instagram, twitter bool) (string, error) {
+func InsertUser(db *sql.DB, firstName, lastName, email, username string, instagram, twitter, github bool) (string, error) {
 
 	sqlStatement := `
-  INSERT INTO users (first_name, last_name, email, username, instagram, twitter)
-  VALUES ($1, $2, $3, $4, $5, $6)
+  INSERT INTO users (first_name, last_name, email, username, instagram, twitter, github)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
   RETURNING id`
 
 	var id []uint8
-	err := db.QueryRow(sqlStatement, firstName, lastName, email, username, instagram, twitter).Scan(&id)
+	err := db.QueryRow(sqlStatement, firstName, lastName, email, username, instagram, twitter, github).Scan(&id)
 	if err != nil {
 		return "", err
 	}

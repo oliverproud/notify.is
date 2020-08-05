21ecf4f1c146e9e562121cf0d14c8c1e149bbd6d
Oliver Proud <oliverproud@users.noreply.github.com>
2020-08-05 13:58:29 +0100

    update database functions to only require first name

--- database/insertUsers.go
+++ database/insertUsers.go
@@ -5,15 +5,15 @@ import (
 )
 
 //InsertUser inserts a new user into the database table 'users'
-func InsertUser(db *sql.DB, firstName, lastName, email, username string, instagram, twitter, github bool) error {
+func InsertUser(db *sql.DB, firstName, email, username string, instagram, twitter, github bool) error {
 
 	sqlStatement := `
-  INSERT INTO users (first_name, last_name, email, username, instagram, twitter, github)
-  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  INSERT INTO users (first_name, email, username, instagram, twitter, github)
+  VALUES ($1, $2, $3, $4, $5, $6)
   RETURNING id`
 
 	var id []uint8
-	err := db.QueryRow(sqlStatement, firstName, lastName, email, username, instagram, twitter, github).Scan(&id)
+	err := db.QueryRow(sqlStatement, firstName, email, username, instagram, twitter, github).Scan(&id)
 	if err != nil {
 		return err
 	}

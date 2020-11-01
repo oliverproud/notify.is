d3fc1af55a8f92b88a8bcbba7ce14acdebe9c792
Oliver Proud <oliverproud@users.noreply.github.com>
2020-11-01 12:46:09 +0000

    automatically copy email to clipboard

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,30 @@
 import Head from "next/head";
 import Link from "next/link";
+import Fade from "react-bootstrap/Fade";
 import Layout from "../components/layout";
+import Overlay from "react-bootstrap/Overlay";
+import Tooltip from "react-bootstrap/Tooltip";
 import IntroHeader from "../components/introHeader";
+import { useState, useEffect, useRef } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+const state = {
+  value: "support@notify.is",
+  copied: false,
+};
+
 
 export default function Contact() {
+
+  const [copied, setCopied] = useState(false);
+  const target = useRef(null);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setCopied(false);
+    }, 3000);
+  });
+
   return (
     <Layout>
       <Head>
@@ -17,10 +38,21 @@ export default function Contact() {
             <h1 className="display-4">Contact</h1>
             <p>Send us an email:</p>
             <h1>
-              <a className="a-contact" href="mailto:support@notify.is">
-                support@notify.is
-              </a>
-            </h1>
+            <CopyToClipboard text={state.value} onCopy={() => setCopied(true)}>
+              <span ref={target}>
+                <a href="#" className="a-contact">support@notify.is</a>
+              </span>
+            </CopyToClipboard>
+          </h1>
+          <Fade in={copied}>
+            <Overlay target={target.current} show={copied} placement="bottom">
+              {(props) => (
+                <Tooltip id="success-tooltip" {...props}>
+                  Copied to clipboard! 🎉
+                </Tooltip>
+              )}
+            </Overlay>
+          </Fade>
           </div>
 
           <style jsx>
